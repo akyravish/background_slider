7bed38328c06e78f2206b9b78e5d72ce90e27a42
akyravish <info.akshayravish@gmail.com>
2021-05-23 08:09:51 +0530

    add further code to change the slides

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const body = document.body;
 const slides = document.querySelectorAll('.slide');
 const lArrow = document.getElementById('left');
-const RArrow = document.getElementById('right');
+const rArrow = document.getElementById('right');
 
 let slideCount = 0;
 
@@ -11,4 +11,31 @@ const bcSlideChange = () => {
 	body.style.backgroundImage = slides[slideCount].style.backgroundImage;
 };
 
+const slideChange = () => {
+	slides.forEach((slide) => slide.classList.remove('active'));
+
+	slides[slideCount].classList.add('active');
+};
+
 bcSlideChange();
+slideChange();
+
+rArrow.addEventListener('click', () => {
+	slideCount++;
+	if (slideCount > slides.length - 1) {
+		slideCount = 0;
+	}
+
+	bcSlideChange();
+	slideChange();
+});
+
+lArrow.addEventListener('click', () => {
+	slideCount--;
+	if (slideCount < 0) {
+		slideCount = slides.length - 1;
+	}
+
+	bcSlideChange();
+	slideChange();
+});
